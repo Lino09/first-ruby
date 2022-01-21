5c9e468818d0e2a94058495ffec5fa2d3689e84d
Abel <aherrera@wootbit.io>
2022-01-21 11:42:19 -0600

    Add enumerable module

--- my_list.rb
+++ my_list.rb
@@ -1,13 +1,19 @@
 require_relative 'my_enumerable'
 
 class MyList
-# include MyEnumerable
-  @list = []
-  def def initialize(list)
-    @list = list
+  include MyEnumerable
+  def initialize(*elements)
+    @list = []
   end
 
   def each(&block)
-    list.map(&block) 
+    list.map(&block)
   end
 end
+
+# list = MyList.new(1, 2, 3, 4)
+# puts list.all? { |e| e < 5 }
+# puts list.all? { |e| e > 5 }
+# puts list.any? { |e| e == 2 }
+# puts list.any? { |e| e == 5 }
+# print list.filter { |e| e.even? }
